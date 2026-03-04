2be800d75fe9aadd6fcbc288f51dceb233c3f075
sklad3737 <sklad3737@gmail.com>
2026-03-04 15:22:58 +0500

    config.py

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("8685523885AAHrCdbbi67sidWIhJ94ji5oyGrfgXba284")
-DATABASE_URL = os.getenv("8685523885AAHrCdbbi67sidWIhJ94ji5oyGrfgXba284")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+DATABASE_URL = os.getenv("BOT_TOKEN")
