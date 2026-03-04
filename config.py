2898cbc29475b878d6946cd0069cfba1fb584ae4
sklad3737 <sklad3737@gmail.com>
2026-03-04 14:14:17 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("8685523885AAHrCdbbi67sidWIhJ94ji5oyGrfgXba284")
+DATABASE_URL = os.getenv("8685523885AAHrCdbbi67sidWIhJ94ji5oyGrfgXba284")
---
+++ main.py
@@ -0,0 +1,18 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+from config import 8685523885AAHrCdbbi67sidWIhJ94ji5oyGrfgXba284
+
+bot = Bot(token=8685523885AAHrCdbbi67sidWIhJ94ji5oyGrfgXba284)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start_handler(message: Message):
+    await message.answer("Бот Фарм-Контроль запущен.")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
